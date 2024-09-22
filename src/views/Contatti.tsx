d5a725503037a6c2cdcd50c0f06ee732c1683e21
Linda Scarano <lindascarano@gmail.com>
2024-09-22 21:20:29 +0200

    fix: Add contacts in Contact view

--- src/views/Contatti.tsx
+++ src/views/Contatti.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { ButtonHome } from "../components/ButtonHome";
 import Contact from "../components/Contact";
 
@@ -12,6 +13,7 @@ export function Contatti() {
     
       <TitleBox prop={TextBoxInterfacemock[0]}></TitleBox>
       <Contact list={ContactListInterfacemock[1]}></Contact>
+      <Typography variant="body1" sx={{textAlign:'center'}}><b>Linda Scarano</b><br/>391.1472953<br/>lindascarano@gmail.com</Typography>
       <ButtonHome></ButtonHome>
     </>
   );
